fcd01aeaf07b79dfc7d116c810deeae95149307a
GGautamKumar <167999668+GGautamKumar@users.noreply.github.com>
2025-03-11 17:24:11 +0530

    Update publicroute.js

--- src/middleware/publicroute.js
+++ src/middleware/publicroute.js
@@ -3,7 +3,6 @@ import { UserContext } from "../Context/Clientcontext";
 import { useNavigate } from "react-router-dom";
 
 
-
 export default function Publicroute(){
     const {user} = useContext(UserContext);
     const navigate = useNavigate();
