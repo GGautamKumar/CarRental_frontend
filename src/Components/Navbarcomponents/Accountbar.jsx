44213846672d61e481c00bd7e0ac3d849178547d
GGautamKumar <167999668+GGautamKumar@users.noreply.github.com>
2025-03-11 17:29:35 +0530

    Update Accountbar.jsx

--- src/Components/Navbarcomponents/Accountbar.jsx
+++ src/Components/Navbarcomponents/Accountbar.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../Context/Clientcontext';
 
 
-
 export default function Accountbar(){
 
     const [isToggle, setIstoggle] = useState(false);
